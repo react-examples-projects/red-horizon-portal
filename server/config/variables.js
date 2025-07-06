4ed1d588676824c40c6299119dea6d57364fcb99
znareak <libardojesusrengifo129@gmail.com>
2025-07-06 19:17:00 +0200

    cors

--- server/config/variables.js
+++ server/config/variables.js
@@ -14,7 +14,7 @@ const SERVER = {
     ALLOWED_DOMAINS: [
       "http://localhost:3000",
       "http://localhost:8080",
-      "https://red-horizon-portal.vercel.app",
+      "https://proyecto-mision-sucre.vercel.app",
     ],
     RATE_LIMITS: {
       windowMs: 10 * 60 * 1000, // 10 minutes
